421d231e18a2c2fbdf4adf339d90e743b1bd8b38
zhanghan9797 <90969686+zhanghan9797@users.noreply.github.com>
2022-05-09 19:19:40 +0800

    Update eval.sh

--- commands/eval.sh
+++ commands/eval.sh
@@ -1,16 +1,32 @@
-###################################### Eval ################################
-#dataset="Dataset name like MSMARCO, TREC_DL."
+################################## Compress ################################
+dataset="Dataset name like MSMARCO, TREC_DL, TREC_COVID."
+query_embed_path="Query embedding path"
+doc_embed_path="Document embedding path"
+checkpoint="Checkpoint path"
+output_dim="Output dimension"
+output_path="Compressed embedding path"
+
+compress_cmd="\
+python ../get_embbedings/get_emb.py --dataset $dataset --query_embed_path $query_embed_path --doc_embed_path $doc_embed_path \
+                             --checkpoint $checkpoint --output_dim $output_dim --output_path $output_path --evaluate\
+"
+
+echo $compress_cmd
+eval $compress_cmd
+
+################################## Compress_NQ ################################
 #query_embed_path="Query embedding path"
 #doc_embed_path="Document embedding path"
-#checkpoint="Checkpoint path or PCA model path"
-#model="Model name like ConAE, KL, CE, PCA"
+#checkpoint="Checkpoint path"
+#nq_test_path="NQ test dataset queries and answers path"
 #output_dim="Output dimension"
+#output_path="Compressed embedding path"
 #
-#eval_cmd="\
-#python ../evaluation/eval_msmarco.py --dataset  $dataset --query_embed_path $query_embed_path --doc_embed_path $doc_embed_path \
-#                             --checkpoint $checkpoint --model $model --output_dim $output_dim\
+#compress_cmd="\
+#python ../get_embbedings/get_nq_emb.py --query_embed_path $query_embed_path --doc_embed_path $doc_embed_path \
+#                             --checkpoint $checkpoint --nq_test_path $nq_test_path \
+#                             --output_dim $output_dim --output_path $output_path --evaluate
 #"
 #
-#echo $eval_cmd
-#eval $eval_cmd
-
+#echo $compress_cmd
+#eval $compress_cmd
