ef039728f11f452809b974ce8b6514b5e08c3c20
zhanghan9797 <90969686+zhanghan9797@users.noreply.github.com>
2022-05-09 15:44:16 +0800

    Update encode.sh

--- commands/encode.sh
+++ commands/encode.sh
@@ -1,22 +1,21 @@
-##################################### Get MSMARCO&TRECCOVID Embeddings ################################
-#output_path="../data/datasets/msmarco/embbedings/"
-#dataset=MSMARCO
-#
+##################################### Get MSMARCO & TRECCOVID Embeddings ################################
+dataset=MSMARCO
+output_path="../data/datasets/msmarco/embbedings/"
+
+emb_cmd="\
+python ../encode/get_dense_emb.py --dataset $dataset --output_path $output_path\
+"
+
+echo $emb_cmd
+eval $emb_cmd
+##################################### Get trecdl Embeddings ################################
+#output_path="../data/datasets/trec_dl/embbedings"
+#query_path="../data/datasets/trec_dl/2019qrels-pass.txt"
 #
 #emb_cmd="\
-#python ../encode/get_dense_emb.py --output_path $output_path\
+#python ../encode/get_trecdl_emb.py --query_path $query_path --output_path $output_path\
 #"
 #
 #echo $emb_cmd
 #eval $emb_cmd
 
-#################################### Get trecdl Embeddings ################################
-output_path="../data/datasets/trec_covid/embbedings"
-query_path="../data/datasets/trec_covid/2019qrels-pass.txt"
-
-emb_cmd="\
-python ../encode/get_trecdl_emb.py --output_path $output_path\
-"
-
-echo $emb_cmd
-eval $emb_cmd
