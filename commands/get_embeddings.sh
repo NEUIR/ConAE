40239e0998efd6dac1ac26942b4d38c67259f888
zhanghan9797 <90969686+zhanghan9797@users.noreply.github.com>
2022-05-09 16:18:41 +0800

    Update get_embeddings.sh

--- commands/get_embeddings.sh
+++ commands/get_embeddings.sh
@@ -23,7 +23,7 @@ eval $compress_cmd
 #output_path="Compressed embedding path"
 #
 #compress_cmd="\
-#python ../get_embbedings/get_emb.py --query_embed_path $query_embed_path --doc_embed_path $doc_embed_path \
+#python ../get_embbedings/get_nq_emb.py --query_embed_path $query_embed_path --doc_embed_path $doc_embed_path \
 #                             --checkpoint $checkpoint --nq_test_path $nq_test_path \
 #                             --output_dim $output_dim --output_path $output_path
 #"
